82272a6157799317d1f348d2aa295a47a56262fb
Mark Collin <mark@ardescosolutions.com>
2012-02-20 16:52:53 +0000

    Holding documentation page added. Tabs move as page changes. Sidebar is now dynamic and set per page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,24 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.ArrayList;
+
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+    public static Result index() {
+        ArrayList sidebarElements = new ArrayList();
+        sidebarElements.add("twitter");
+        sidebarElements.add("links");
+        return ok(index.render(sidebarElements));
+    }
+
+    public static Result documentation() {
+        ArrayList sidebarElements = new ArrayList();
+        sidebarElements.add("twitter");
+        return ok(documentation.render(sidebarElements));
+    }
+
 }
